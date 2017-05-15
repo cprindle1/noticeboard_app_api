baee749b457f13566e60c67b3cf2da3e5a0880ce
555032 <charlesprindle@new-host-2.home>
2017-05-15 16:32:52 -0400

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,6 +11,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :update, :delete, :options, :head]
+      methods: [:get, :options, :head]
+  end
+  allow do
+    origins 'localhost:8000',
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
